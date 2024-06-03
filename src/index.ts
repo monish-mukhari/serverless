0d4a5136b9ec8fcf03cf812493faf7a19068d7b4
monish-mukhari <manishmukherjee91@gmail.com>
2024-06-03 20:40:11 +0530

    init

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,18 @@
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		console.log(request.body);
+		console.log(request.headers);
+
+		if(request.method == "GET"){
+			return Response.json({
+				message: "you sent a get request"
+			});
+		} else {
+			return Response.json({
+				message: "you did not sent a get request"
+		})
+	
+		};
 	},
 };
